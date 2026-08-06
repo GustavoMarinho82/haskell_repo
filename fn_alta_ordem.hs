d6a3b378ba598df71de9e3417bb29a02f91bac87
GustavoMarinho82 <gustavo.mariinho06@gmail.com>
2026-08-05 21:44:30 -0300

    funções de alta ordem

---
+++ fn_alta_ordem.hs
@@ -0,0 +1,22 @@
+lista = [1..10]
+
+mapa = map (\x -> x*2) lista
+
+filtro = filter (\x -> x `mod` 2 == 0) lista
+
+lista2 = [1, 2, 3]
+
+fold_direita = foldr (\x y -> x - y) 0 lista2
+-- [1, 2, 3, 0] -> [1, 2, 3 (3-0)] -> [1, -1 (2-3)] -> 2 (1+1)
+
+foldl_esquerda = foldl (\x y -> x - y) 0 lista2
+-- [0, 1, 2, 3] -> [-1 (0-1), 2, 3] -> [-3 (-1-2), 3] -> -6 (-3-3)
+
+main :: IO()
+main = do
+    putStrLn ("lista -> " ++ show lista)
+    putStrLn ("map -> " ++ show mapa)
+    putStrLn ("filter -> " ++ show filtro)
+    putStrLn ("lista2 -> " ++ show lista2)
+    putStrLn ("foldr -> " ++ show fold_direita)
+    putStrLn ("foldl -> " ++ show foldl_esquerda)
