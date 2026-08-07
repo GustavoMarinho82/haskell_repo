a5b81ca30ab4af19e3ec688a101c778a1ebdb97e
GustavoMarinho82 <gustavo.mariinho06@gmail.com>
2026-08-07 18:22:44 -0300

    let e patt_match

--- EDL/A1.hs
+++ EDL/A1.hs
@@ -1,9 +1,9 @@
-fn_aux :: Int -> [(Int, Int)] -> [(Int, Int)]
-fn_aux x [] = [(0, x)]
-fn_aux x acc = (fst (head acc) + 1, 10^(fst (head acc) + 1)*x) : acc
+indices :: Int -> [(Int, Int)] -> [(Int, Int)]
+indices x [] = [(0, x)]
+indices x acc = (fst (head acc) + 1, x) : acc
 
 lista_to_num :: [Int] -> Int
-lista_to_num l = foldr (\x acc -> snd x + acc) 0 (foldr (fn_aux) [] l)
+lista_to_num l = foldr (\(i, x) acc -> (x * 10^i) + acc) 0 (foldr (indices) [] l)
 
 main :: IO ()
 main = do
--- EDL/A5.hs
+++ EDL/A5.hs
@@ -4,10 +4,9 @@ mapf f l = foldr (\x acc -> (f x) : acc) [] l
 filterf :: (a -> Bool) -> [a] -> [a]
 filterf f l = foldr (\x acc -> if (f x) then x : acc else acc) [] l
 
-lista = [1..10]
-
 main :: IO()
-main = do
+main = let lista = [1..10] in 
+    do 
     print (map (\x -> x*2) lista)
     print (mapf (\x -> x*2) lista)
     print (filter (\x -> x `mod` 2 == 0) lista)
